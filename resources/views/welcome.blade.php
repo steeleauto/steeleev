a328e8f525ca6522f990397d223fd8895f97512e
= <=>
2020-01-28 17:14:06 -0400

    Tweak to homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Steele EV Site Soon
+                    SteeleAuto EV
                 </div>
 
                 <div class="links">
