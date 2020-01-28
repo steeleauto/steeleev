308d7389efa3007b34eeada0c6139467091fe949
= <=>
2020-01-28 16:34:36 -0400

    Setting Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Steele EV
                 </div>
 
                 <div class="links">
